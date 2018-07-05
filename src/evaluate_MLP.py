00b02a1b91d28f3c0e544d538b69643973877abe
Somin Wadhwa <sominwadhwa@gmail.com>
2018-07-05 20:21:41 +0530

    extract features

--- src/evaluate_MLP.py
+++ src/evaluate_MLP.py
@@ -1,105 +1,23 @@
-import json   #parse json
-import spacy  #tokenizing text
-import progressbar
-import pickle as pk
-import scipy.io
-from spacy.en import English
-from sklearn.externals import joblib
-from sklearn.preprocessing import LabelEncoder
-from utils import most_freq_answer, grouped, get_questions_sum
-from keras.models import Sequential
 import sys
-from keras.layers.core import Dense, Dropout, Activation
-#def main():
+import argparse
 
-try:
-    f1 = open("/home/raunaq/VQAMD/preprocessed/results.txt","w")
-    questions_val = open("/home/raunaq/VQAMD/preprocessed/ques_val.txt","r").read()
-    answers_val = open("/home/raunaq/VQAMD/preprocessed/answer_val.txt",'r').read()
-    image_ids = open("/home/raunaq/VQAMD/preprocessed/images_coco_id_val.txt",'r').read()
-    vgg_path = "/home/raunaq/vgg_feats.mat"
+from progressbar import Bar, ETA, Percentage, ProgressBar    
+from keras.models import model_from_json
+#from spacy.en import English
+import numpy as np
+import scipy.io
+from sklearn.externals import joblib
 
-    answers_train = open("../preprocessed/answer_train.txt","rb").read().decode('utf8').splitlines()
+from extract_features import get_questions_matrix_sum, get_images_matrix, get_answers_matrix
+from utils import grouped
 
-except IOError:
-    print ("Error in opening files")
+def main():
 
-vgg_features = scipy.io.loadmat(vgg_path)
-img_features = vgg_features['feats']
-print ("Loaded Vgg features")
+    model = model_from_json(open('baseline_mlp.json').read())
+    model.load_weights('weights/MLP_epoch_99.hdf5')
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-try:
-    label_encoder = joblib.load("/home/raunaq/VQAMD/preprocessed/label_encoder.sav")
-except:
-    print ("Label encoder didn't load")
+    print ("Model Loaded with Weights")
 
-nlp = English()
-print ("Loaded word2vec")
-weights_path = "MLP_epoch_60.hdf5"
-
-lbl = LabelEncoder()
-lbl.fit(answers_train)
-nb_classes = len(list(lbl.classes_))
-y_predict_text = []
-widgets = ['Evaluating ', progressbar.Percentage(), ' ', progressbar.Bar(marker='#',left='[',right=']'),
-       ' ', progressbar.ETA()]
-pbar = progressbar.ProgressBar(widgets=widgets)
-
-#Model Parameters
-num_hidden_units = 1024
-num_hidden_layers = 3
-batch_size = 256
-dropout = 0.5
-activation = 'tanh'
-img_dim = 4096
-word2vec_dim = 300
-num_epochs = 100
-log_interval = 10
-
-# Recreating the model architecture
-model = Sequential()
-model.add(Dense(num_hidden_units, input_dim=word2vec_dim+img_dim, kernel_initializer='uniform'))
-model.add(Dropout(dropout))
-for i in range(num_hidden_layers):
-    model.add(Dense(num_hidden_units, kernel_initializer='uniform'))
-    model.add(Activation(activation))
-    model.add(Dropout(dropout))
-model.add(Dense(nb_classes, kernel_initializer='uniform'))
-model.add(Activation('softmax'))
-
-# Loading weights
-model.load_weights(weights_path)
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-
-for qu_batch,an_batch in pbar(zip(grouped(questions_val, batch_size, fillvalue=questions_val[0]),
-                                            grouped(answers_val, batch_size, fillvalue=answers_val[0]))):
-                                            X_batch = get_questions_sum(qu_batch, nlp)
-                                            y_predict = model.predict_classes(X_batch, verbose=0)
-                                            y_predict_text.extend(labelencoder.inverse_transform(y_predict))
-
-for prediction, truth, question, image in zip(y_predict_text, answers_val, questions_val):
-		temp_count=0
-		for _truth in truth.split(';'):
-			if prediction == _truth:
-				temp_count+=1
-
-		if temp_count>2:
-			correct_val+=1
-		else:
-			correct_val+= float(temp_count)/3
-
-		total+=1
-		f1.write(question.encode('utf-8'))
-		f1.write('\n')
-		f1.write(prediction)
-		f1.write('\n')
-		f1.write(truth.encode('utf-8'))
-		f1.write('\n')
-
-f1.write('Final Accuracy is ' + str(correct_val/total))
-f1.close()
-f1 = open('../results/overall_results.txt', 'a')
-f1.write(args.weights + '\n')
-f1.write(str(correct_val/total) + '\n')
-f1.close()
-print ('Final Accuracy on the validation set is', correct_val/total)
+if __name__ == '__main__':
+    main()
---
+++ src/extract_features.py
@@ -0,0 +1,84 @@
+import numpy as np
+from keras.utils import np_utils
+
+
+def get_questions_tensor_timeseries(questions, nlp, timesteps):
+	'''
+	Returns a time series of word vectors for tokens in the question
+	Input:
+	questions: list of unicode objects
+	nlp: an instance of the class English() from spacy.en
+	timesteps: the number of 
+	Output:
+	A numpy ndarray of shape: (nb_samples, timesteps, word_vec_dim)
+	'''
+	assert not isinstance(questions, basestring)
+	nb_samples = len(questions)
+	word_vec_dim = nlp(questions[0])[0].vector.shape[0]
+	questions_tensor = np.zeros((nb_samples, timesteps, word_vec_dim))
+	for i in xrange(len(questions)):
+		tokens = nlp(questions[i])
+		for j in xrange(len(tokens)):
+			if j<timesteps:
+				questions_tensor[i,j,:] = tokens[j].vector
+
+	return questions_tensor
+
+def get_questions_matrix_sum(questions, nlp):
+	'''
+	Sums the word vectors of all the tokens in a question
+	
+	Input:
+	questions: list of unicode objects
+	nlp: an instance of the class English() from spacy.en
+	Output:
+	A numpy array of shape: (nb_samples, word_vec_dim)	
+	'''
+	assert not isinstance(questions, basestring)
+	nb_samples = len(questions)
+	word_vec_dim = nlp(questions[0])[0].vector.shape[0]
+	questions_matrix = np.zeros((nb_samples, word_vec_dim))
+	for i in xrange(len(questions)):
+		tokens = nlp(questions[i])
+		for j in xrange(len(tokens)):
+			questions_matrix[i,:] += tokens[j].vector
+
+	return questions_matrix
+
+def get_answers_matrix(answers, encoder):
+	'''
+	Converts string objects to class labels
+	Input:
+	answers:	a list of unicode objects
+	encoder:	a scikit-learn LabelEncoder object
+	Output:
+	A numpy array of shape (nb_samples, nb_classes)
+	'''
+	assert not isinstance(answers, basestring)
+	y = encoder.transform(answers) #string to numerical class
+	nb_classes = encoder.classes_.shape[0]
+	Y = np_utils.to_categorical(y, nb_classes)
+	return Y
+
+def get_images_matrix(img_coco_ids, img_map, VGGfeatures):
+	'''
+	Gets the 4096-dimensional CNN features for the given COCO
+	images
+	
+	Input:
+	img_coco_ids: 	A list of strings, each string corresponding to
+				  	the MS COCO Id of the relevant image
+	img_map: 		A dictionary that maps the COCO Ids to their indexes 
+					in the pre-computed VGG features matrix
+	VGGfeatures: 	A numpy array of shape (nb_dimensions,nb_images)
+	Ouput:
+	A numpy matrix of size (nb_samples, nb_dimensions)
+	'''
+	assert not isinstance(img_coco_ids, basestring)
+	nb_samples = len(img_coco_ids)
+	nb_dimensions = VGGfeatures.shape[0]
+	image_matrix = np.zeros((nb_samples, nb_dimensions))
+	for j in xrange(len(img_coco_ids)):
+		image_matrix[j,:] = VGGfeatures[:,img_map[img_coco_ids[j]]]
+
+	return image_matrix
